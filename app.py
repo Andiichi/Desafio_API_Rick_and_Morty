c79d366168b11b59fd03dc992f8d28eff8c6a404
Andiichi <andrezza.n.dasilva@gmail.com>
2024-08-27 22:38:42 -0300

    update de algumas correcoes e paginas

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
-from flask import Flask, render_template, jsonify, request
-import requests, urllib.request, json
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+import requests, json
 
 app = Flask(__name__)
 
 @app.route('/')
 def get_list_characters_page():
-    # Renderiza a primeira página de personagens
     url = 'https://rickandmortyapi.com/api/character/'
-    response = urllib.request.urlopen(url)
-    data = response.read()
-    data_dict = json.loads(data)
+    response = requests.get(url)
+    data_dict = response.json()
 
     return render_template('characters.html', characters=data_dict['results'])
 
 @app.route('/load_more', methods=['GET'])
 def load_more():
-    # Retorna os personagens de uma página específica como JSON
     page = request.args.get('page', 1, type=int)
     url = f'https://rickandmortyapi.com/api/character/?page={page}'
     response = requests.get(url)
@@ -25,27 +22,60 @@ def load_more():
 @app.route('/profile/<id>')
 def get_profile(id):
     url = f'https://rickandmortyapi.com/api/character/{id}'
-    response = urllib.request.urlopen(url)
-    data = response.read()
-    data_dict = json.loads(data)
+    response = requests.get(url)
+    data_dict = response.json()
 
     return render_template('profile.html', profile=data_dict)
 
 @app.route('/episodes')
 def get_episodes():
     url = 'https://rickandmortyapi.com/api/episode/'
-    response = urllib.request.urlopen(url)
-    data = response.read()
-    data_dict = json.loads(data)
+    response = requests.get(url)
+    data_dict = response.json()
 
     return render_template('episodes.html', episodes=data_dict['results'])
+
+
+
+@app.route('/locations')
+def get_locations():
+    url = 'https://rickandmortyapi.com/api/location/'
+    response = requests.get(url)
+    data_dict = response.json()
+
+    return render_template('locations.html', locations=data_dict['results'])
+
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('query', '').lower()
+
+    # Verifica se a busca é um número (possivelmente um ID)
+    if query.isdigit():
+        # Redireciona diretamente para a página de perfil do personagem
+        return redirect(url_for('get_profile', id=query))
+    
+    # Caso contrário, realiza a busca por nome
+    if query:
+        url = f'https://rickandmortyapi.com/api/character/?name={query}'
+        response = requests.get(url)
+        data_dict = response.json()
+
+        # Verifica se encontrou personagens
+        if 'results' in data_dict:
+            return render_template('characters.html', characters=data_dict['results'])
+        else:
+            return render_template('characters.html', characters=[], message="Nenhum personagem encontrado.")
+    else:
+        return redirect('/')
+
+
+
 
 @app.route('/lista')
 def get_list_characters():
     url = 'https://rickandmortyapi.com/api/character/'
-    response = urllib.request.urlopen(url)
-    characters = response.read()
-    characters_dict = json.loads(characters)
+    response = requests.get(url)
+    characters_dict = response.json()
 
     characters = []
 
@@ -75,3 +105,95 @@ def get_list_characters():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+# from flask import Flask, render_template, jsonify, request
+# import requests, urllib.request, json
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def get_list_characters_page():
+#     url = 'https://rickandmortyapi.com/api/character/'
+#     response = urllib.request.urlopen(url)
+#     data = response.read()
+#     data_dict = json.loads(data)
+
+#     return render_template('characters.html', characters=data_dict['results'])
+
+
+
+# @app.route('/load_more', methods=['GET'])
+# def load_more():
+#     # Retorna os personagens de uma página específica como JSON
+#     page = request.args.get('page', 1, type=int)
+#     url = f'https://rickandmortyapi.com/api/character/?page={page}'
+#     response = requests.get(url)
+#     data_dict = response.json()
+#     return jsonify(data_dict['results'])
+
+# @app.route('/profile/<id>')
+# def get_profile(id):
+#     url = 'https://rickandmortyapi.com/api/character/' + id
+#     response = urllib.request.urlopen(url)
+#     data = response.read()
+#     data_dict = json.loads(data)
+
+#     return render_template('profile.html', profile=data_dict)
+
+# @app.route('/episodes')
+# def get_episodes():
+#     url = 'https://rickandmortyapi.com/api/episode/'
+#     response = urllib.request.urlopen(url)
+#     data = response.read()
+#     data_dict = json.loads(data)
+
+#     return render_template('episodes.html', episodes=data_dict['results'])
+
+# @app.route('/lista')
+# def get_list_characters():
+#     url = 'https://rickandmortyapi.com/api/character/'
+#     response = urllib.request.urlopen(url)
+#     characters = response.read()
+#     characters_dict = json.loads(characters)
+
+#     characters = []
+
+#     for character in characters_dict['results']:
+#         character_info = {
+#             'id': character['id'],
+#             'name': character['name'],
+#             'status': character['status'],
+#             'species': character['species'],
+#             'type': character['type'],
+#             'gender': character['gender'],
+#             'origin': {
+#                 'name': character['origin']['name'],
+#                 'url': character['origin']['url'],
+#             },
+#             'location': {
+#                 'name': character['location']['name'],
+#                 'url': character['location']['url'],
+#             },
+#             'image': character['image'],
+#             'episode': character['episode']
+#         }
+    
+#         characters.append(character_info)
+    
+#     return jsonify({'results': characters})
+
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -5,22 +5,29 @@ function loadMoreCharacters(page) {
       let characterContainer = document.getElementById("character-container");
       data.forEach((character) => {
         let characterElement = document.createElement("div");
-        characterElement.classList.add("col-4");
+        characterElement.classList.add("col-sm-3", "col-6", "mb-4"); // Adicionei "mb-4" para margin-bottom
         characterElement.innerHTML = `
-        
-                     <div class="col-sm-2 col-6">
-             
-                    <img src="${character.image}" alt="${character.name}" class=" img-fluid img-thumbnail">
-                    
-                    <h3 class="h5">${character.name}</h3>
-                    <div class="d-grid gap-2 d-md-block">
-                        <a href="/profile/${character.id}" 
-                        class="btn btn-primary">Ver perfil</a>
-                    </div>
-      
-                </div>
-           
-                `;
+        <br>
+        </h3> <!-- Adicionei "mt-2" para margin-top -->
+        <h3 class="h5 mt-2 d-flex justify-content-between">
+            <small> ${character.name}</small>
+          <blockquote class="lead">
+                #${character.id}
+          </blockquote>
+        </h3>
+          <img src="${character.image}" alt="${character.name}" class="img-fluid img-thumbnail">
+          <h3 class="h5 mt-2 d-flex justify-content-between">
+              Status: ${character.status}
+          </h3>
+          <h3 class="h5 mt-2 d-flex justify-content-between">
+              Location: ${character.location['name']}
+          </h3>
+          <p>
+            <a href="/profile/${character.id}" class="btn btn-primary">Ver perfil</a>
+          </p>
+   
+        `
+        ;
         characterContainer.appendChild(characterElement);
       });
     })
@@ -38,3 +45,55 @@ document.addEventListener("DOMContentLoaded", function () {
   // Carrega a primeira página de personagens ao carregar a página
   loadMoreCharacters(page);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function loadMoreCharacters(page) {
+//   fetch(`/load_more?page=${page}`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       let characterContainer = document.getElementById("character-container");
+//       data.forEach((character) => {
+//         let characterElement = document.createElement("div");
+//         characterElement.classList.add("col-sm-3 col-6");
+//         characterElement.innerHTML = `
+            
+
+//         <img src="${character.image}" alt="${character.name}" class=" img-fluid img-thumbnail">
+        
+//         <h3 class="h5">${character.name}</h3>
+//         <p>
+//           <a href="/profile/${character.id}" 
+//           class="btn btn-primary">Ver perfil</a>
+//         </p>
+
+//         `;
+//         characterContainer.appendChild(characterElement);
+//       });
+//     })
+//     .catch((error) => console.error("Error:", error));
+// }
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   let page = 1;
+
+//   document.getElementById("load-more").addEventListener("click", function () {
+//     page++;
+//     loadMoreCharacters(page);
+//   });
+
+//   // Carrega a primeira página de personagens ao carregar a página
+//   loadMoreCharacters(page);
+// });
