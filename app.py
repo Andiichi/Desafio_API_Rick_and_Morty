e0e3a6e1ac024d607fbc8c5c322f6e3d86db6e40
Andiichi <121890466+Andiichi@users.noreply.github.com>
2024-09-06 10:48:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ def get_list_characters_page():
 
     return render_template('characters.html', characters=data_dict['results'])
 
+
 @app.route('/load_more', methods=['GET'])
 def load_more():
     page = request.args.get('page', 1, type=int)
@@ -19,6 +20,7 @@ def load_more():
     data_dict = response.json()
     return jsonify(data_dict['results'])
 
+
 @app.route('/profile/<id>')
 def get_profile(id):
     url = f'https://rickandmortyapi.com/api/character/{id}'
@@ -26,6 +28,7 @@ def get_profile(id):
     data_dict = response.json()
 
     return render_template('profile.html', profile=data_dict)
+
 
 @app.route('/episodes')
 def get_episodes():
@@ -130,97 +133,7 @@ def get_list_characters():
     
     return jsonify({'results': characters})
 
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-# from flask import Flask, render_template, jsonify, request
-# import requests, urllib.request, json
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def get_list_characters_page():
-#     url = 'https://rickandmortyapi.com/api/character/'
-#     response = urllib.request.urlopen(url)
-#     data = response.read()
-#     data_dict = json.loads(data)
-
-#     return render_template('characters.html', characters=data_dict['results'])
-
-
-
-# @app.route('/load_more', methods=['GET'])
-# def load_more():
-#     # Retorna os personagens de uma página específica como JSON
-#     page = request.args.get('page', 1, type=int)
-#     url = f'https://rickandmortyapi.com/api/character/?page={page}'
-#     response = requests.get(url)
-#     data_dict = response.json()
-#     return jsonify(data_dict['results'])
-
-# @app.route('/profile/<id>')
-# def get_profile(id):
-#     url = 'https://rickandmortyapi.com/api/character/' + id
-#     response = urllib.request.urlopen(url)
-#     data = response.read()
-#     data_dict = json.loads(data)
-
-#     return render_template('profile.html', profile=data_dict)
-
-# @app.route('/episodes')
-# def get_episodes():
-#     url = 'https://rickandmortyapi.com/api/episode/'
-#     response = urllib.request.urlopen(url)
-#     data = response.read()
-#     data_dict = json.loads(data)
-
-#     return render_template('episodes.html', episodes=data_dict['results'])
-
-# @app.route('/lista')
-# def get_list_characters():
-#     url = 'https://rickandmortyapi.com/api/character/'
-#     response = urllib.request.urlopen(url)
-#     characters = response.read()
-#     characters_dict = json.loads(characters)
-
-#     characters = []
-
-#     for character in characters_dict['results']:
-#         character_info = {
-#             'id': character['id'],
-#             'name': character['name'],
-#             'status': character['status'],
-#             'species': character['species'],
-#             'type': character['type'],
-#             'gender': character['gender'],
-#             'origin': {
-#                 'name': character['origin']['name'],
-#                 'url': character['origin']['url'],
-#             },
-#             'location': {
-#                 'name': character['location']['name'],
-#                 'url': character['location']['url'],
-#             },
-#             'image': character['image'],
-#             'episode': character['episode']
-#         }
-    
-#         characters.append(character_info)
-    
-#     return jsonify({'results': characters})
-
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
